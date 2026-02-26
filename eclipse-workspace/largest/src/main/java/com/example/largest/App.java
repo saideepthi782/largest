88fe861c4dec90c61875087e92e29fe3b8881088
sai deepthi <saideepthibcse@giet.ac.in>
2026-02-26 19:44:13 +0530

    main

---
+++ eclipse-workspace/largest/src/main/java/com/example/largest/App.java
@@ -0,0 +1,10 @@
+package com.example.largest;
+import java.lang.Math;
+public class App {
+    public static void main(String[] args) {
+    	 double a = 12.123;
+         double b = 12.456;
+         System.out.println(Math.max(a, b));  
+        System.out.println("Hello World!");
+    }
+}
