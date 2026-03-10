e281fa219bed33c0b879302709a2ad81887c1792
saideepthi782 <saideepthibcse@giet.ac.in>
2026-03-10 21:03:49 +0530

    Implement Largest.java to find maximum of two numbers

---
+++ Largest.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+public class App {
+    public static void main(String[] args) {
+    	 double a = 12.123;
+         double b = 12.456;
+         System.out.println(Math.max(a, b));  
+        System.out.println("Hello World!");
+    }
+}
